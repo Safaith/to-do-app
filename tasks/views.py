dd86d306f81c0973a65ef43938c0c7c915d64e6d
Safaith Hasan <safaith895@gmail.com>
2023-09-10 12:38:01 +0600

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+from .forms import taskform
+from .models import task
+
+def index(request):
+    tasks = task.objects.all()
+
+    form = taskform()
+    if request.method=="POST":
+        form=taskform(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+    
+    context = {"form":form,"tasks":tasks}
+    return render(request,"tasks/list.html",context)
+
+def update_task(request,pk):
+   Task = task.objects.get(id=pk)
+
+   form = taskform(instance=Task)
+
+   if request.method=="POST":
+      form = taskform(request.POST, instance=Task)
+      if form.is_valid():
+          form.save()
+          return redirect("/")
+
+   context = {"form":form}
+
+   return render(request,"tasks/update_task.html",context)
+
+def delete_task(request,pk):
+    item = task.objects.get(id=pk)
+
+    if request.method=="POST":
+        item.delete()
+        return redirect("/")
+    context = {"item":item}
+    return render(request,"tasks/delete_task.html",context)
+
